510b809950d3df1fe9c9d8bb08f90c111c9bc422
mattjaf <mattjaf@gmail.com>
2023-02-12 18:21:34 -0800

    added events, _albumCover, and getAllAlbumMetaData

--- smartContracts/NFTMusicFactory.sol
+++ smartContracts/NFTMusicFactory.sol
@@ -14,8 +14,36 @@ contract NFTMusicFactory is Ownable {
      * Could implement a Admin only modifier
      * Possibly launch a interface contract and make copies of different logic
      */
+
+    event PublishedAblum(
+        address indexed contractAddress,
+        uint256 indexed contractIndex,
+        string[] songURIs, //is this a bit much?
+        string indexed name,
+        string symbol,
+        string albumCover //couldn't this be extracted from the first NFT's metadata
+    );
+
+    event BurnAlbum(
+        address indexed contractAddress,
+        uint256 indexed contractIndex
+    );
+
+    event BurnSong(
+        address indexed contractAddress,
+        uint256 indexed contractIndex,
+        uint256 indexed tokenId
+    );
+
+    struct AlbumMetaData {
+        string title;
+        string albumCover;
+        address contractAddress;
+    }
+
     bool public publicMintStatus;
     NFTMusicSimple[] private NFTMusicSimpleArray;
+    uint256 albumCounter; //might not need this
 
     constructor() {
         publicMintStatus = true;
@@ -28,32 +56,80 @@ contract NFTMusicFactory is Ownable {
     function deployNFTMusicSimple(
         string memory _name,
         string memory _symbol,
-        string[] memory _songs
+        string[] memory _songs,
+        string memory _albumCover
     ) public {
         require(
             publicMintStatus || owner() == msg.sender,
             "Public mint is closed"
         );
+        albumCounter++;
         NFTMusicSimple NFTMusicSimpleContract = new NFTMusicSimple(
             _name,
             _symbol,
             _songs,
-            address(this)
+            address(this),
+            _albumCover
         );
         NFTMusicSimpleArray.push(NFTMusicSimpleContract);
+        emit PublishedAblum(
+            address(NFTMusicSimpleContract),
+            albumCounter,
+            _songs, //is this a bit much?
+            _name,
+            _symbol,
+            _albumCover
+        );
     }
 
     function burnAlbum(uint256 _index) public onlyOwner {
         NFTMusicSimpleArray[_index].burnAlbum();
+        emit BurnAlbum(address(NFTMusicSimpleArray[_index]), _index);
     }
 
     function burnSong(uint256 _index, uint256 _tokenId) public onlyOwner {
         NFTMusicSimpleArray[_index].burnSong(_tokenId);
+        emit BurnSong(address(NFTMusicSimpleArray[_index]), _index, _tokenId);
     }
 
     //////////////////////
     // GETTER FUNCTIONS //
     //////////////////////
+
+    function getAlbumMetaDataByIndex(uint256 _index)
+        public
+        view
+        returns (AlbumMetaData memory)
+    {
+        AlbumMetaData memory metaData;
+        (
+            metaData.title,
+            metaData.albumCover,
+            metaData.contractAddress
+        ) = NFTMusicSimpleArray[_index].albumMetaData();
+        return metaData;
+    }
+
+    //these function increase the code size to 26638
+    function getAllAlbumMetaData()
+        public
+        view
+        returns (AlbumMetaData[] memory)
+    {
+        AlbumMetaData[] memory metaDataArray = new AlbumMetaData[](
+            totalSupply()
+        );
+        uint256 counter = 0;
+        for (uint256 i = 0; i < totalSupply(); i++) {
+            (
+                metaDataArray[counter].title,
+                metaDataArray[counter].albumCover,
+                metaDataArray[counter].contractAddress
+            ) = NFTMusicSimpleArray[i].albumMetaData();
+            counter++;
+        }
+        return metaDataArray;
+    }
 
     function whoOwns(uint256 _index) public view returns (address) {
         return NFTMusicSimpleArray[_index].owner();
