3a3feca599b6e4189336415af3622edfc74c4e76
mattjaf <mattjaf@gmail.com>
2023-03-05 15:10:17 -0800

    updated cloud functions for mumbai redeployment

--- CloudFunctions/cloud.js
+++ CloudFunctions/cloud.js
@@ -1,6 +1,7 @@
-const axios = require('axios');
+const axios = require("axios");
 
-Moralis.Cloud.afterSave("PublishedAblum", async (request) => { // spelled wrong in contract fix on redeployment
+Moralis.Cloud.afterSave("PublishedAlbum", async (request) => { //fixed spelling
+    // spelled wrong in contract fix on redeployment
     const confirmed = request.object.get("confirmed");
     const logger = Moralis.Cloud.getLogger();
     logger.info("Looking for confirmed transaction...");
@@ -12,7 +13,7 @@ Moralis.Cloud.afterSave("PublishedAblum", async (request) => { // spelled wrong 
         const txHash = request.object.get("transactionHash");
         const contractAddress = request.object.get("contractAddress");
         const contractIndex = request.object.get("contractIndex");
-        const name = request.object.get("name"); //changed to albulmName on redeployment
+        const albumName = request.object.get("albumName"); //changed to albulmName on redeployment
         const symbol = request.object.get("symbol");
         const albumCover = request.object.get("albumCover");
         const songURIs = request.object.get("songURIs");
@@ -21,7 +22,7 @@ Moralis.Cloud.afterSave("PublishedAblum", async (request) => { // spelled wrong 
         library.set("transactionHash", txHash);
         library.set("contractAddress", contractAddress);
         library.set("contractIndex", contractIndex);
-        library.set("albumName", name); //this is emiting the wrong data for some reason?
+        library.set("albumName", albumName); //this is emiting the wrong data for some reason? b.c its indexed
         library.set("symbol", symbol);
         library.set("albumCover", albumCover);
         library.set("songURIs", songURIs);
@@ -41,6 +42,7 @@ Moralis.Cloud.afterSave("PublishedAblum", async (request) => { // spelled wrong 
 
                 songObj.set("albumName", metadata.album);
                 songObj.set("songName", metadata.name);
+                songObj.set("contractAddress", contractAddress); //added
                 songObj.set("uri", song);
                 songObj.set("animation_url", metadata.animation_url);
                 songObj.set("publisher", metadata.publisher);
@@ -49,6 +51,7 @@ Moralis.Cloud.afterSave("PublishedAblum", async (request) => { // spelled wrong 
                 songObj.set("genre", metadata.description);
                 songObj.set("duration", metadata.duration);
                 songObj.set("symbol", metadata.symbol);
+                songObj.set("chainId", metadata.chainId); // added
                 songObj.set("metadata", metadata);
 
                 songObj.set("library", library); // set up the relationship
@@ -57,17 +60,17 @@ Moralis.Cloud.afterSave("PublishedAblum", async (request) => { // spelled wrong 
             }
 
             const firstSongMetadata = songDataArray[0];
-            library.set("albumName", firstSongMetadata.name); //delete this and check value on redeployment
+            //library.set("albumName", firstSongMetadata.album); //delete this and check value on redeployment
             library.set("publisher", firstSongMetadata.publisher);
             library.set("artist", firstSongMetadata.artist);
             library.set("year", firstSongMetadata.year);
             library.set("title", firstSongMetadata.name);
             library.set("genre", firstSongMetadata.description);
+            library.set("chainId", firstSongMetadata.chainId); // added
             library.set("metadata", songDataArray);
 
             await library.save();
             logger.info("Metadata saved.");
-
         } catch (error) {
             logger.error(`Error extracting metadata: ${error}`);
         }
